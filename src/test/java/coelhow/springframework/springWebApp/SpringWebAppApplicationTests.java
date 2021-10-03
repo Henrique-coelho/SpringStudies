9a73879991d3fdbaffd1a48a7a98fd1b45d2175d
Henrique-coelho <h8mendes@gmail.com>
2021-10-03 12:44:27 -0300

    Commit inicial

---
+++ src/test/java/coelhow/springframework/springWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package coelhow.springframework.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
